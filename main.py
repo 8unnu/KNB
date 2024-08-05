f04c3f4579ab567e22ca55e2afb58b3f9f6abb6d
8unnu <samsdnkind0707@gmail.com>
2024-08-05 15:26:51 +0000

    1.3 - Mini visual fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 
-from pydantic import BaseModel
+
 
 import random
 
@@ -17,20 +17,6 @@ app.mount('/static', StaticFiles(directory='static'))
 if __name__ == "__main__":
     uvicorn.run("main:app")
 
-# async def game(sign):
-#     arr = ['stone', 'scissors', 'paper']
-#     ai_sign = arr[random.randint(0, 2)]
-#     if ai_sign == sign:
-#         return "Draw"
-#     elif ai_sign == "stone" and sign == "paper":
-#         return "You win"
-#     elif ai_sign == "scissors" and sign == "stone":
-#         return "You win"
-#     elif ai_sign == "paper" and sign == "scissors":
-#         return "You win"
-#     else:
-#         return "You lose"
-
 async def sign_converter(sign):
     if sign == "✂️":
         return "scissors"
@@ -38,6 +24,14 @@ async def sign_converter(sign):
         return "stone"
     elif sign == "🧻":
         return "paper"
+
+async def reverse_sign_converter(sign):
+    if sign == "scissors":
+        return "✂️"
+    elif sign == "stone":
+        return "🪨"
+    elif sign == "paper":
+        return "🧻"
 @app.get('/')
 async def index(request: Request):
     return templates.TemplateResponse(request, 'index.html')
@@ -59,9 +53,16 @@ async def game(request: Request, sign: str = Form()):
         result = "You win"
     else:
         result = "You lose"
+
+    sign2 = sign
+    ai_sign2 = ai_sign
+    ai_sign = await reverse_sign_converter(ai_sign)
+    sign = await reverse_sign_converter(sign)
+
     context = {
         'request': request,
-        'result': f'{result}, you take {sign} and ai take {ai_sign}'
+        'result': f'{result}, you take {sign} ({sign2}) and ai take {ai_sign} ({ai_sign2})',
+        'score': "0:0"
     }
 
     return templates.TemplateResponse('index.html', context=context)
